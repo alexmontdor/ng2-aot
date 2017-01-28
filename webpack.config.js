12d95f9de5984c6bdd3ccf912938a2db7b5308b8
Alexandre LANDREAU <alexandre@landreau.biz>
2017-01-28 14:26:14 -0800

    Change to use ng 2.4.5 and aot + compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+var webpack = require('webpack');
+
 const ngToolsWebpack = require('@ngtools/webpack');
+
+var CompressionPlugin = require("compression-webpack-plugin");
+
 const path = require("path");
 
 module.exports = {
@@ -15,7 +20,23 @@ module.exports = {
   plugins: [
     new ngToolsWebpack.AotPlugin({
       tsConfigPath: './tsconfig.aot.json'
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            },
+            sourceMap: false
+        }),
+      new CompressionPlugin({
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: /\.js$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.8
+      })
   ],
   module: {
     loaders: [
